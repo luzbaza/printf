e4ee8f218a005b33c27f312e19a7463e398f6a84
luzbaza <2459@holbertonschool.com>
2021-03-20 02:35:57 -0500

    holberton

---
+++ holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct datatype - Struct datatype
+ *
+ * @type: The type of argument
+ * @func: The function pointer to the print function
+ */
+typedef struct datatype
+{
+	char *type;
+	int (*func)(va_list list);
+
+} data_t;
+
+int _putchar(char c);
+int _printchar(va_list ap);
+int _printstring(va_list ap);
+int _printint(va_list ap);
+int _printf(const char *format, ...);
+
+#endif /* HOLBERTON_H */
